45c1374bc2f860090a73fc0ff9d1cbd26139bd1c
浩瀚猫 <howcanoewang@gmail.com>
2025-03-06 15:33:31 +0900

    rm old tk easyams

--- uitests.py
+++
@@ -1,7 +0,0 @@
-# ensure using <conda:lab> with Metashape.whl installed 
-from easyams.utils import ImgLoaderPanel
-
-if __name__ == "__main__":
-    # test img loader panel
-    ilp = ImgLoaderPanel()
-    ilp.mainloop()
